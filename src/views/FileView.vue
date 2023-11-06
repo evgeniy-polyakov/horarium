5b4bfc725576d31c85d71833e49830a5ddfaac1c
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2023-11-06 11:59:33 +0300

    Nested routes

---
+++ src/views/FileView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>File {{ $route.params.file }}</h2>
+  <RouterView/>
+</template>
---
+++ src/views/TableView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Table {{ $route.params.table }}</h2>
+</template>
