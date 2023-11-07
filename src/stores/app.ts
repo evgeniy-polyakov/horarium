8217a3e2a92ef1c060115665aaeb9d2eb49ab186
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2023-11-08 00:51:05 +0300

    File view

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,6 +1,6 @@
 import {defineStore} from 'pinia'
 import {FileTabItem} from "@/components/FileTabItem";
-import {reactive, ref, watch} from "vue";
+import {reactive, watch} from "vue";
 import {useRoute} from "vue-router";
 import {TableTabItem} from "@/components/TableTabItem";
 import {AppFile} from "@/models/AppFile";
@@ -8,20 +8,18 @@ import {AppFile} from "@/models/AppFile";
 export const useAppStore = defineStore('app', () => {
     const fileTabItems = reactive<FileTabItem[]>([]);
     const tableTabItems = reactive<TableTabItem[]>([]);
-    const selectedFileTab = ref<FileTabItem | undefined>(undefined);
 
     const route = useRoute();
     watch(
         () => route.fullPath,
         () => {
-            const selectedFileTabValue = fileTabItems.filter(it => it.selected)[0];
-            selectedFileTab.value = selectedFileTabValue;
+            const fileTab = fileTabItems.filter(it => it.selected)[0];
             tableTabItems.splice(0, tableTabItems.length);
-            if (selectedFileTabValue) {
-                tableTabItems.push(...selectedFileTabValue.file.database.getTables().map(table => new TableTabItem(selectedFileTabValue.file as AppFile, table)));
+            if (fileTab) {
+                tableTabItems.push(...fileTab.file.database.getTables().map(table => new TableTabItem(fileTab.file as AppFile, table)));
             }
         }
     );
 
-    return {fileTabItems, selectedFileTab, tableTabItems};
+    return {fileTabItems, tableTabItems};
 })
--- src/views/FileView.vue
+++ src/views/FileView.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
 import {useAppStore} from "@/stores/app";
 import Tabs from "@/components/Tabs.vue";
+import router from "@/router";
+import {TableTabItem} from "@/components/TableTabItem";
 
-const fileTab = useAppStore().selectedFileTab;
 const items = useAppStore().tableTabItems;
+
+const onEditTableName = (item: TableTabItem, value: string) => {
+  const link = item.link;
+  item.label = value;
+  if (link !== item.link) {
+    item.table.name = value;
+    router.replace(item.link);
+  }
+};
+
+const onRemoveTable = (index: number) => {
+  // todo confirmation
+  // if (fileTab) {
+  //
+  // }
+  // fileTab?.file.database.deleteTable();
+  // items.splice(index, 1);
+
+};
 
 </script>
 
 <template>
-  <Tabs v-if="fileTab" :items="items"/>
+  <Tabs :items="items"
+        @edit-tab="onEditTableName"
+        @remove-tab="onRemoveTable"/>
   <RouterView/>
 </template>
