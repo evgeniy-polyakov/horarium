6b6ecd9f2336d2cd477e16b4aa7abdabdfd40a01
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2023-11-07 09:37:09 +0300

    Moved all code to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,48 @@
 <script setup lang="ts">
+import Tabs from "@/components/Tabs.vue";
+import IconButton from "@/components/IconButton.vue";
+import OpenFileButton from "@/components/OpenFileButton.vue";
+import {AppFile} from "@/models/AppFile";
+import {FileTabItem} from "@/components/FileTabItem";
+import {ITabItem} from "@/components/ITabItem";
+import router from "@/router";
+import {useAppStore} from "@/stores/app";
+
+const items = useAppStore().fileTabItems;
+
+const onOpenFile = async (file: AppFile) => {
+  let item = items.filter(it => it.file.filename === file.filename)[0];
+  if (!item) {
+    await file.parse();
+    item = new FileTabItem(file);
+    items.push(item);
+  }
+  await router.push(item.link);
+};
+
+const onEditFilename = (item: ITabItem, value: string) => {
+  const link = item.link;
+  item.label = value;
+  if (link !== item.link) {
+    router.replace(item.link);
+  }
+};
+
+const onRemoveFile = (index: number) => {
+  items.splice(index, 1);
+};
+
 </script>
 
 <template>
-  <RouterView/>
+  <header>
+    <OpenFileButton @open="onOpenFile"/>
+    <IconButton icon="file-download"/>
+    <Tabs :items="items" fixed
+          @remove-tab="onRemoveFile"
+          @edit-tab="onEditFilename"/>
+  </header>
+  <main>
+    <RouterView/>
+  </main>
 </template>
