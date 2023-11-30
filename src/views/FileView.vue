4437d2967fed2e4d7a21ea89be9cb1f878d6c024
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2023-11-30 22:34:25 +0300

    Text area in file view

--- src/views/FileView.vue
+++ src/views/FileView.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
-import Tabs from "@/components/Tabs.vue";
+import {ref, watch} from "vue";
+import {useAppStore} from "@/stores/app";
+
+const scrollTopByFile: Record<string, number> = {};
+const scrollLeftByFile: Record<string, number> = {};
+
+const store = useAppStore();
+const text = ref("");
+const filename = ref("");
+let textarea = ref<HTMLTextAreaElement>();
+
+watch(text, value => {
+  store.getSelectedFile().textContent = value;
+});
+
+store.watchSelectedFile(file => {
+  text.value = file.textContent;
+  filename.value = file.filename;
+  const ta = textarea.value;
+  if (ta) {
+    ta.scrollTop = scrollTopByFile[file.filename] ?? 0;
+    ta.scrollLeft = scrollLeftByFile[file.filename] ?? 0;
+  }
+});
 
 </script>
 
 <template>
-<!--  <RouterView/>-->
-<!--  <Tabs :items="items"-->
-<!--        @edit-tab="onEditTableName"-->
-<!--        @remove-tab="onRemoveTable"/>-->
+  <textarea ref="textarea" v-model="text" @scroll="() => {
+    scrollTopByFile[filename] = textarea?.scrollTop ?? 0;
+    scrollLeftByFile[filename] = textarea?.scrollLeft ?? 0;
+  }"></textarea>
 </template>
