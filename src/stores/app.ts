33ab84ea98d2d37e6fffc4a922c7c2255eec0916
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2023-11-30 22:33:20 +0300

    Watching selected file

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,8 +1,31 @@
 import {defineStore} from 'pinia'
 import {FileTabItem} from "@/components/FileTabItem";
-import {reactive} from "vue";
+import {reactive, watch} from "vue";
+import {FileModel} from "@/models/FileModel";
 
 export const useAppStore = defineStore('app', () => {
     const fileTabItems = reactive<FileTabItem[]>([]);
-    return {fileTabItems};
-})
+    const selectedFileWatchers: { (file: FileModel): void }[] = [];
+
+    const getSelectedFile = () => fileTabItems.filter(it => it.selected)[0]?.file as FileModel;
+
+    watch(
+        getSelectedFile,
+        file => {
+            if (file) {
+                selectedFileWatchers.forEach(it => it(file as FileModel));
+            }
+        });
+
+    const watchSelectedFile = (watcher: (file: FileModel) => void) => {
+        if (selectedFileWatchers.indexOf(watcher) < 0) {
+            selectedFileWatchers.push(watcher);
+            const file = getSelectedFile();
+            if (file) {
+                watcher(file);
+            }
+        }
+    };
+
+    return {fileTabItems, watchSelectedFile, getSelectedFile};
+});
