93b9427a9276ad173a5744b95aa469ce24615548
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-01-11 11:11:15 +0300

    Fixed number of columns

--- src/components/TableEditor.tsx
+++ src/components/TableEditor.tsx
@@ -7,14 +7,21 @@ export function TableEditor({file}: {
 }) {
     const [textContent] = useState("");
     const [csv, setCSV] = useState<string[][]>([]);
+    let columns = 0;
     if (textContent !== file.textContent) {
         parse(file.textContent, {
             relaxColumnCount: true,
             relaxQuotes: true,
-        }, (err, records, info) => {
+        }, (err, records: string[][]) => {
             if (!err) {
+                columns = records.reduce((a, t) => Math.max(t.length, a), 0);
+                records.forEach(it => {
+                    while (it.length < columns) it.push("");
+                });
                 setCSV(records);
             } else {
+                columns = 0;
+                setCSV([]);
                 console.error(err);
             }
         });
