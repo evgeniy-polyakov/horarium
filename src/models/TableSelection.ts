a75c148a7ab52c5cbd3944ab9910eca67372fc06
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-01-17 12:53:31 +0300

    Selection of table cells ranges

--- src/models/TableSelection.ts
+++ src/models/TableSelection.ts
@@ -28,9 +28,24 @@ class SelectionRange {
         return this.inRange(rowIndex, cellIndex);
     }
 
-    exclude(rowIndex: number, cellIndex: number) {
-        if (this.inRange(rowIndex, cellIndex)) {
-            this.excluded.push(rowIndex, cellIndex);
+    exclude(range: SelectionRange): void;
+    exclude(rowIndex: number, cellIndex: number): void;
+    exclude(rowIndex: number | SelectionRange, cellIndex?: number): void {
+        if (typeof rowIndex === "number" && typeof cellIndex === "number") {
+            if (this.contains(rowIndex, cellIndex)) {
+                this.excluded.push(rowIndex, cellIndex);
+            }
+        } else if (typeof rowIndex === "object") {
+            const range = rowIndex;
+            const minRow = Math.min(range.startRow, range.endRow);
+            const maxRow = Math.max(range.startRow, range.endRow);
+            const minCell = Math.min(range.startCell, range.endCell);
+            const maxCell = Math.max(range.startCell, range.endCell);
+            for (let row = minRow; row <= maxRow; row++) {
+                for (let cell = minCell; cell <= maxCell; cell++) {
+                    this.exclude(row, cell);
+                }
+            }
         }
     }
 
@@ -38,6 +53,10 @@ class SelectionRange {
         this.endRow = endRow;
         this.endCell = endCell;
         this.excluded = [];
+    }
+
+    isEmpty() {
+        return this.excluded.length >= (1 + Math.abs(this.startRow - this.endRow)) * (1 + Math.abs(this.startCell - this.endCell)) * 2;
     }
 
     private inRange(rowIndex: number, cellIndex: number) {
@@ -61,7 +80,9 @@ export class TableSelection {
             if (this.ranges.length === 0) {
                 this.ranges.push(new SelectionRange(rowIndex, cellIndex, rowIndex, cellIndex));
             } else {
-                this.ranges[this.ranges.length - 1].expand(rowIndex, cellIndex);
+                const range = this.ranges[this.ranges.length - 1];
+                range.expand(rowIndex, cellIndex);
+                this.swallowRanges(range);
             }
         } else if (mode & MODE_RANGE) {
             if (this.ranges.length === 0) {
@@ -72,13 +93,29 @@ export class TableSelection {
             }
         } else if (mode & MODE_APPEND) {
             if (mode & MODE_SELECT) {
-                this.ranges.push(new SelectionRange(rowIndex, cellIndex, rowIndex, cellIndex));
+                const range = new SelectionRange(rowIndex, cellIndex, rowIndex, cellIndex);
+                this.ranges.push(range);
+                this.swallowRanges(range);
             } else {
                 this.ranges.forEach(it => it.exclude(rowIndex, cellIndex));
+                this.clearRanges();
             }
         } else {
             this.ranges = [new SelectionRange(rowIndex, cellIndex, rowIndex, cellIndex)];
         }
+    }
+
+    swallowRanges(range: SelectionRange) {
+        this.ranges.forEach(it => {
+            if (it !== range) {
+                it.exclude(range);
+            }
+        })
+        this.clearRanges();
+    }
+
+    clearRanges() {
+        this.ranges = this.ranges.filter(it => !it.isEmpty());
     }
 }
 
