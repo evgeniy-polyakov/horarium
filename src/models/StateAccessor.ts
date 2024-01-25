45f2382a7977037ea3a9ec29b8849dc0e2700c04
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-01-25 12:59:47 +0300

    Update of state accessor

--- src/models/StateAccessor.ts
+++ src/models/StateAccessor.ts
@@ -1,18 +1,35 @@
-import {useState} from "react";
+import {Dispatch, SetStateAction, useState} from "react";
 
-export interface StateAssessor<T> {
-    get(): T;
-    set(value: T): void;
-}
+export type StateAssessor<S> = [S, Dispatch<SetStateAction<S>>] & {
+    (value: S): void;
+    readonly value: S;
+    /**
+     * @deprecated
+     */
+    get(): S;
+    /**
+     * @deprecated
+     * @param value
+     */
+    set(value: S): void;
+};
 
-export function useStateAccessor<T>(value: T | { (): T }): StateAssessor<T> {
-    const state = useState(value);
-    return {
-        get() {
-            return state[0];
-        },
-        set(value: T) {
-            state[1](value);
+export function useStateAccessor<S>(initialState: S | (() => S)): StateAssessor<S> {
+    const [value, setter] = useState(initialState);
+    const accessor = function (value: S) {
+        setter(value);
+    } as any;
+    accessor.get = () => value;
+    accessor.set = (value: S) => setter(value);
+    Object.defineProperty(accessor, "value", {value, writable: false, enumerable: false, configurable: false});
+    Object.defineProperty(accessor, "0", {value, writable: false, enumerable: false, configurable: false});
+    Object.defineProperty(accessor, "1", {value: setter, writable: false, enumerable: false, configurable: false});
+    Object.defineProperty(accessor, Symbol.iterator, {
+        writable: false, enumerable: false, configurable: false,
+        value: function* () {
+            yield value;
+            yield setter;
         }
-    }
+    });
+    return accessor;
 }
